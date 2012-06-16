46dcc0b25eb9a23bf6d58fb186381fc728f7da34
Chris Maddox <tyre77@gmail.com>
2012-06-16 17:41:45 -0400

    updates files uploaded

--- app/controllers/uploaders_controller.rb
+++ app/controllers/uploaders_controller.rb
@@ -3,12 +3,13 @@ class UploadersController < ApplicationController
   def show
     @room_id = params[:room_id]
     @user_token = params[:user_token]
+    @uploaded_files = Upload.where(user_token: @user_token, room_id: @room_id)
   end
 
   def create
     if params[:file]
       file = params[:file]
-      upload = Upload.new(params[:data])
+      upload = Upload.new(user_token: params[:file_user_token], room_id: params[:file_room_id])
       upload.upload_to_s3(file.original_filename, file)
       upload.save
       render json: { status: :created, url: upload.url }
