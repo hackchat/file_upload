83cc50035251463d4742edc6c8a09cc62a9f1939
Chris Maddox <tyre77@gmail.com>
2012-06-16 22:04:43 -0400

    set default permissions to public_read so peeps can use files

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,7 +2,7 @@ class Upload < ActiveRecord::Base
   attr_accessible :room_id, :url, :user_token
 
   def upload_to_s3(file_name, file)
-     FileUploader.store(file_name, file, CURRENT_BUCKET)
+     FileUploader.store(file_name, file, CURRENT_BUCKET, access: :public_read)
      self.url = "http://#{CURRENT_BUCKET}.s3.amazonaws.com/#{file_name}"
   end
 end
